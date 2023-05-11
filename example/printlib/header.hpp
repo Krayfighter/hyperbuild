055250da4abddb6927ce2563114283e7bcf76b93
Aiden J Kring <aidenjkring@gmail.com>
2023-05-11 15:41:30 -0400

    seperately compiled internal library

---
+++ example/printlib/header.hpp
@@ -0,0 +1,6 @@
+
+// #include <cstdlib>
+#include <iostream>
+// #include <random>
+
+void print_something_new();
---
+++ example/printlib/lib.cpp
@@ -0,0 +1,6 @@
+
+#include "header.hpp"
+
+void print_something_new() {
+    std::cout << "this is printed from the printlib library" << std::endl;
+}
