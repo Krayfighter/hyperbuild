2bf43ca8633623af5f305157022f42e64cede505
Aiden J Kring <aidenjkring@gmail.com>
2023-05-11 15:41:15 -0400

    add external function

--- example/main.cpp
+++ example/main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 #include "helper.hpp"
+#include "printlib/header.hpp"
 
 int main() {
     std::cout << "C++ code" << std::endl;
     print_one_thing();
     print_many_things();
+    print_something_new();
 }
